bd71687c104a05288a423e4f3262243623d80d66
ijassper <jasspercoach@gmail.com>
2026-07-30 18:43:24 +0900

    스트림릿 언어 배우기
    스트림릿의 입출력 함수와 파이썬의 입출력함수 비교

---
+++ app_chatbot.py
@@ -0,0 +1,12 @@
+# python 입출력함수
+# -> 입력함수 input()
+# -> 출력함수 print()
+
+# streamlit 입출력함수
+# -> 입력함수 text_input(), number_input()
+# -> 출력함수 write()
+
+import streamlit
+
+print("Hello, world")
+streamlit.write("Hi, World")
