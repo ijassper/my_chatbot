b8552fb3618aa9a1f9666099e19f77d3307619c5
ijassper <jasspercoach@gmail.com>
2026-08-06 18:32:12 +0900

    Update app_chatbot.py

--- app_chatbot.py
+++ app_chatbot.py
@@ -18,13 +18,14 @@ st.write("Hi, World")
 
 # 텍스트박스 입력함수 input("")
 # input("이름을 입력하세요: ")
-name = st.text_input("이름을 입력하세요: ")
+name = st.text_input("이름을 입력하세요")
+age = st.number_input("나이를 입력하세요")
 
 if name:
   #출력
   # 000님 반갑습니다.
-  st.write(f"{name}님 반갑습니다.")
+  st.write(f"{age}살, {name}님 반갑습니다.")
   # 자료형 문자, 숫자, 불연산자, 튜플, 리스트, 딕셔너리
-  st.write(type(name))
+  st.write(type(age))
 #else:
   #미출력
