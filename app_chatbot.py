c9b52f2a123a8dbf7936f6521f5de83b8f02fa92
ijassper <jasspercoach@gmail.com>
2026-08-13 18:50:34 +0900

    Refactor weather API integration and user input

--- app_chatbot.py
+++ app_chatbot.py
@@ -7,6 +7,7 @@
 # -> 출력함수 write()
 
 import streamlit as st
+import requests
 
 #print("Hello, world")
 #st.write("Hi, World")
@@ -26,13 +27,13 @@ st.title("날씨 챗봇")
 
 # 텍스트박스 입력함수 input("")
 # input("이름을 입력하세요: ")
-col1, col2 = st.columns(2)
+#col1, col2 = st.columns(2)
 
-with col1:
-  name = st.text_input("이름을 입력하세요")
+#with col1:
+#  name = st.text_input("이름을 입력하세요")
   # age = st.number_input("나이를 입력하세요")
 
-  if name:
+#  if name:
     #출력
     # 000님 반갑습니다.
     #st.write(f"{age}살, {name}님 반갑습니다.")
@@ -42,32 +43,44 @@ with col1:
     #st.write(type(age))
   #else:
     #미출력
-with col2:  
-  st.subheader("날씨를 알아볼까요?")
-  if st.button("클릭하세요."):
-    option = st.selectbox("날씨를 선택하세요",["맑음","흐림","비"])
+#with col2:  
+#  st.subheader("날씨를 알아볼까요?")
+#  if st.button("클릭하세요."):
+#    option = st.selectbox("날씨를 선택하세요",["맑음","흐림","비"])
     #weather = 
-    st.write(f"날씨 선택 : {option}")
+#    st.write(f"날씨 선택 : {option}")
 
-# 1. API 기본 설정
-api_key = "9D4OIHOT-9D4O-9D4O-9D4O-9D4OIHOTZC"
-base_url = "https://www.safemap.go.kr/openApiService/wms/getLayerData.do"
+# 날씨 API 설정
+SERVICE_KEY = "3c2a5f26fb58a9dc3506acf29da6d160442693bbba7ec23b401c1367712f80e5"
+BASE_URL = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"  # 단기예보조회
 
-# 2. 사용자 입력 (지도 조회를 위한 파라미터)
-st.subheader("지도 조회 설정")
-# 실제 서비스에서는 레이어명을 선택하게 하면 좋습니다.
-layer_name = st.text_input("레이어명 (예: A2SM_TFCACDSTTUS_BIG)", "A2SM_TFCACDSTTUS_BIG")
+nx = st.text_input("지역의 X좌표를 입력하세요 (예: 60")
+ny = st.text_input("지역의 Y좌표를 입력하세요 (예: 60")
 
-if st.button("지도 불러오기"):
-    # 3. WMS 방식의 URL 생성
-    # 참고: 이미지 예시에 나온 파라미터 조합
-    wms_url = f"{base_url}?apikey={api_key}&LAYERS={layer_name}&STYLES={layer_name}&FORMAT=image/png&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&CRS=EPSG:4326&BBOX=33,124,43,132&WIDTH=500&HEIGHT=500"
-    
-    st.write("생성된 URL:", wms_url)
-    
-    # 4. 이미지 출력
-    try:
-        st.image(wms_url, caption="생활안전지도 데이터 시각화")
-        st.success("데이터를 성공적으로 불러왔습니다!")
-    except Exception as e:
-        st.error(f"이미지를 불러오는 중 오류가 발생했습니다: {e}")
+if st.button("날씨 확인"):
+  # API로 데이터 보내기
+  parmas = {
+    'serviceKey': SERVICE_KEY,
+    'pageNo': '1',
+    'numOfRows': '10',
+    'dataType': 'JSON',
+    'base_date': '20260813',
+    'base_time': '0700',
+    'nx': nx,
+    'ny': ny,
+  }
+
+  # 요청한 API 데이터 받기
+  response = requests.get(BASE_URL, params=params)
+  data = response.json()
+
+  # 받은 정보 JSON 구조 분석
+  try:
+    items = data['response']['body']['items']['item']
+    st.write("##현재 날씨 정보")
+    for item in items:
+      category = item['category']
+      value = item['fcsValue']
+      st.write(f"- {category}: {value}")
+  except KeyError:
+    st.error("데이터를 가져올 수 없습니다.")
