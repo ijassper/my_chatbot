825cb094e99302061e0cf3e4fb01818c3dd6f14d
ijassper <jasspercoach@gmail.com>
2026-08-27 19:01:44 +0900

    Refactor weather input and response display
    Commented out the line that displays the weather response and moved the user input box to the top.

--- op_weather.py
+++ op_weather.py
@@ -7,14 +7,14 @@ API_KEY = "8464cff4dd30fe09c5447727526f939a"
 st.title("우리동네 날씨챗봇")
 st.write("도시 이름을 잊력하면 현재 날씨를 알려드려요.")
 
+# 사용자 입력박스
+city = st.text_input("도시 이름을 영어로 입력하세요 (예:Seoul,Busan)","Seoul")
+
 def get_weather(city_name):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric&lang=kr"
     response = requests.get(url)
-    st.write(response.json())
-    #return response.json()
-
-# 사용자 입력박스
-city = st.text_input("도시 이름을 영어로 입력하세요 (예:Seoul,Busan)","Seoul")
+    #st.write(response.json())
+    return response.json()
 
 # 날씨를 검색할 도시를 입력받으면 검색 시작
 if city:    
