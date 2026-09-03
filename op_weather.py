eb95a1a13083913edad6aed2118b04a31419a8fc
ijassper <jasspercoach@gmail.com>
2026-09-03 19:05:39 +0900

    Comment out weather output and API key display
    Comment out API key and response output for debugging.

--- op_weather.py
+++ op_weather.py
@@ -11,10 +11,10 @@ st.write("도시 이름을 잊력하면 현재 날씨를 알려드려요.")
 city = st.text_input("도시 이름을 영어로 입력하세요 (예:Seoul,Busan)","Seoul")
 
 def get_weather(city_name):
-    st.write(city_name,API_KEY)
+    #st.write(city_name,API_KEY)
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric&lang=kr"
     response = requests.get(url)
-    st.write(response.json())
+    #st.write(response.json())
     return response.json()
 
 # 날씨를 검색할 도시를 입력받으면 검색 시작
@@ -26,5 +26,5 @@ if city:
     st.write(f"{city}의 날씨를 알려드릴게요.")
     # 온도, 날씨(맑음)
     st.write(f"온도 : {weather_data['main']['temp']} C")
-    st.write(f"날씨 : {weather_data['weather'][0]['description']}")
+    #st.write(f"날씨 : {weather_data['weather'][0]['description']}")
 
