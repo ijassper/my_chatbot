2a7fcd9548fc9473b1d0bdf884e1ac014065ec7f
ijassper <jasspercoach@gmail.com>
2026-09-10 17:59:00 +0900

    Uncomment weather data display in op_weather.py

--- op_weather.py
+++ op_weather.py
@@ -20,7 +20,7 @@ def get_weather(city_name):
 # 날씨를 검색할 도시를 입력받으면 검색 시작
 if city:    
     weather_data = get_weather(city)
-    #st.write(weather_data)
+    st.write(weather_data)
 
     # 도시출력
     st.write(f"{city}의 날씨를 알려드릴게요.")
